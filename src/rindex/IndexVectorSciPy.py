627395a594af6269440f46dd0531326b15fdf99b
tobiasw225 <tobiasw225@gmail.com>
2017-05-04 17:31:02 +0200

    tobis kleiner beitrag

--- src/rindex/IndexVectorSciPy.py
+++ src/rindex/IndexVectorSciPy.py
@@ -1,42 +1,115 @@
 import numpy as np
 import scipy.sparse as sp
 import random
+import hashlib
+import itertools
+import nltk
+# from scipy.signal.ltisys import dimpulse
 
-from scipy.signal.ltisys import dimpulse
+## :8 because seeding allows only 'small' values
+## looks like we don't nee this. have to check wether hash-function is the same
+## on every system!
+string_to_hash = lambda input: int(hashlib.md5(input.encode('utf-8')).hexdigest()[:8],16)
+
+## it looks like the usual hash-function generates new values every time.
+#tuple_to_hash = lambda input: hash(tuple(input)) % 4294967295
+
 
 
 class IndexVector:
 
-	vec = sp.coo_matrix
-	randomIndexDB = {}
-	dim = 10
-	n = 2
+    vec = sp.coo_matrix
+    randomIndexDB = {}
+    dim = 10 # default
+    # Anzahl -1,1
+    n = 2 # default
+    prob_minus = prob_plus = prob_zero = 0.0
+
+    def __init__(self, dim, n):
+        self.dim = dim
+        self.n = n
+        self.setProb()
+        
+    def setProb(self):
+        ## only needed for tobi's 'hashing'
+        self.prob_zero = (self.dim-self.n)/self.dim
+        self.prob_minus = self.prob_plus = (self.n/self.dim)/2
 
 
-	def __init__(self, dim, n):
-		self.dim = dim
-		self.n = n
+            
+    def createIndexVectorFromContext(self, context=[]):
+        """
+            Creates an Index-Vector given a context array with words.
+            __Notes__:
+            seeding with same array -> same value
+            could be done in a similar way janos did in createIndexVectorRandom
+            :param context[]:
+        """
+        ## summieren des "gekappten" hashs
+        self.vec = sp.csr_matrix((self.dim,1))
+        for word in context:
+            np.random.seed(string_to_hash(word))
+            ## Offensichtlich hatte ich das doch nicht ganz verstanden:
+            ## Jedes Element wird mit einer Wahrscheinlichkeit ausgewählt.
+            ## -> funktioniert natürlich trotzdem.
+            self.vec += sp.csr_matrix(np.random.choice([0, 1, -1], size=(self.dim,1),
+                                                  p=[self.prob_zero, self.prob_plus, self.prob_minus]))
+        return self.vec
 
-	def createIndexVectorFromContext(self, context=[]):
-		#self.vec = tf.SparseTensor(indices=[[0, 0], [1, 2]], values=[1, 2], dense_shape=[3, 4])
-		return self.vec
+        
+    def createIndexVectorRandom(self, context=[]):
+        ## is this the place where you choose the indexes
+        ## or rather how do you save the values?
 
-	def createIndexVectorRandom(self, context=[]):
-		row = np.array([random.randint(0, self.dim-1) for x in range(self.n)])
-		col = np.array([0 for x in range(self.n)])
-		values = np.array([random.choice([-1, 1]) for x in range(self.n)])
-		self.vec = sp.coo_matrix((values, (row,col)), (self.dim, 1))
-		return self.vec
+        ## This works because random generates the same sequence of values after
+        ## the same seed. After all, I like this method better because we have full
+        ## control about the count of -1 and 1s.
+        ## -> might be faster, too
+        np.random.seed(string_to_hash(context[0])) ##
+        
+        row = np.array([np.random.randint(0, self.dim-1) for x in range(self.n)])
+        col = np.array([0 for x in range(self.n)])
+        
 
-	def set(self, dimension=0, n=0):
-		self.dim = dimension
-		self.n = n
+        values = np.array([np.random.choice([-1, 1]) for x in range(self.n)])
+        self.vec = sp.coo_matrix((values, (row,col)), (self.dim, 1))
+        return self.vec
+
+    def set(self, dimension=0, n=0):
+        self.dim = dimension
+        self.n = n
 
 
 def main():
-	i = IndexVector(100,2)
-	i.createIndexVectorRandom()
+    iv = IndexVector(100,3)
 
+    ## festlegen, dass nur float
+    testSentence = ["I", "like", "Parks", "and", "Recreation"]
+
+    for word in testSentence + testSentence:
+        iv.createIndexVectorFromContext([word])
+        print(word,iv.vec)
+        print()
+    
+    # for i in range(10):
+    #     iv.createIndexVectorRandom([testSentence[0]])
+    #     print(iv.vec)
+    #     print()
+    ##testHash(testSentence)
+
+    
+    # i.createIndexVectorRandom()
+    # print(i.vec)
+    # print()
+    
+    # i.createIndexVectorFromContext(['hello','world'])
+    # print(i.vec)
+    # print()
+    # i.createIndexVectorFromContext(['world','world'])
+    # print(i.vec)
+    # print()
+    # i.createIndexVectorFromContext(['world','hello'])
+    # print(i.vec)
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/rindex/RIModel.py
+++ src/rindex/RIModel.py
@@ -1,126 +1,256 @@
 import itertools
-
 import scipy.sparse as sp
+from scipy import spatial
+from scipy.stats import entropy
+
 import numpy as np
-from rindex import IndexVectorSciPy as IndexVec
+from numpy.linalg import norm
+import random
+
+import pickle
+# from rindex
+import IndexVectorSciPy as IndexVec
+
+
+
+"""
+As far as I know pickle saves only class objects and not
+complete objects. We could of course call it as a method of RIModel.
+"""
+def writeModelToFile(rmi, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(rmi, output)
+def loadModelFromFile(filename):
+    with open(filename, 'rb') as inputFile:
+        return(pickle.load(inputFile))
+fix_vec = lambda input:[(number,number+0.0001)[number == 0.0] for
+                        number in input]
+
+
+
 
 class RIModel:
+    """RIModel - Random IndexVector Model
+    
+    """
+    ContextVectors = {}
+    iv = IndexVec.IndexVector
+    dim = 0
+    k = 0
+    index_memory = {}
 
-	ContextVectors = {}
-	iv = IndexVec.IndexVector
-	dim = 0
-	k = 0
-
-	index_memory = {}
-
-	def __init__(self, dim=0, k=0):
-		self.dim = dim
-		self.k = k
-		self.iv = IndexVec.IndexVector(dim, k)
-
-
-	def addDocument(self,context=[]):
-		""" 	Takes the Context array as the the context of its entries and each element of the array as word
-		"""
-		for word in context:
-			if word in self.ContextVectors.keys():
-				self.ContextVectors[word] += self.iv.createIndexVectorRandom()
-			else:
-				self.ContextVectors[word] = sp.coo_matrix((self.dim, 1))
-				self.ContextVectors[word] += self.iv.createIndexVectorRandom()
-
-
-	def addContext(self, context = [], index = 0, mask = None):
-		"""Add a self defined Context for a specifix word with index , possibly with a weight mask
-		   default: index = 0 ( the first word in the array ) , if no mask given all contexts are weighted 1"""
-
-		if not mask:
-			mask = [1] * len(context)
-
-		for word, weight in zip(context, mask):
-			if word == context[index]:
-				continue
-
-			if word not in self.index_memory.keys():
-				self.index_memory[word] = self.iv.createIndexVectorRandom()
-
-			if context[index] in self.ContextVectors.keys():
-				self.ContextVectors[context[index]] += weight * self.index_memory[word]
-			else:
-				self.ContextVectors[context[index]] = sp.coo_matrix((self.dim, 1))
-				self.ContextVectors[context[index]] += weight * self.index_memory[word]
+    def __init__(self, dim=0, k=0):
+        self.dim = dim
+        self.k = k
+        self.iv = IndexVec.IndexVector(dim, k)
 
 
 
-	def getSimilarityCos(self, word1, word2):
-		"""	Calculate the Cosine-Similarity between the two elements word1 and word2
-			word1, word2 must occur in the Model
-		:param word1:
-		:param word2:
-		:return:
-		"""
-
-		scalarproduct = (self.ContextVectors[word1].transpose() * self.ContextVectors[word2]).toarray()[0][0]
-		norm1 = (self.ContextVectors[word1].transpose() * self.ContextVectors[word1]).toarray()[0][0]
-		norm2 = (self.ContextVectors[word2].transpose() * self.ContextVectors[word2]).toarray()[0][0]
-		return scalarproduct / (np.sqrt(norm1) * np.sqrt(norm2))
+    def addDocument(self,context=[]):
+        """     Takes the Context array as the context of its entries and each element of the array as word
+        """
+        for word in context:
+            if word in self.ContextVectors.keys():
+                pass
+            else:
+                                self.ContextVectors[word] = sp.coo_matrix((self.dim, 1))
+            self.ContextVectors[word] += self.iv.createIndexVectorFromContext(word)
 
 
-	def mostSimilar(self,count= 10, file = None ):
-		""" Compare all words in model. (Takes long Time)
-		:param count:
-		:param file:
-		:return:
-		"""
-
-		simDic = []
-		keys = self.ContextVectors.keys()
-		tuples = list(itertools.combinations(keys, 2))
-		print("Comparing everything...")
-
-		i = 0
-		size = len(tuples)
-		for pair in tuples:
-			i+=1
-
-			print("\r%f %%" % (100*i/size), end="")
-			simDic.append([pair[0], pair[1], self.getSimilarityCos(pair[0], pair[1])])
-
-		print("Sorting...")
-		simDic.sort(key=lambda x: x[2], reverse=True)
-		for x in range(count):
-			print(x, ":\t", simDic[x])
-
-		if file:
-			with open(file, 'w') as out:
-				for triple in simDic:
-					out.write(triple[0] + "\t" + triple[1] + "\t" + str(triple[2]) + "\n")
+    def addContext(self, context = [], index = 0, mask = None):
+        """Add a self defined Context for a specifix word with index , possibly with a weight mask
+           default: index = 0 ( the first word in the array ) , if no
+        mask given all contexts are weighted 1
+        :param context[]:
+        :param index:
+        :param mask:
+        """
+        if not mask:
+            mask = [1] * len(context)
+        for word, weight in zip(context, mask):
+        
+            if word == context[index]:
+                continue
+            if word not in self.index_memory.keys():
+                                self.index_memory[word] = self.iv.createIndexVectorFromContext([word])
+            if context[index] not in self.ContextVectors.keys():
+                                self.ContextVectors[context[index]] = sp.coo_matrix((self.dim, 1))
+            self.ContextVectors[context[index]] += self.index_memory[word] * weight
 
 
+
+    def getSimilarityCos(self, word1, word2):
+         """    Calculate the Cosine-Similarity between the two elements word1 and word2
+         word1, word2 must occur in the Model
+         :param word1:
+         :param word2:
+         :return:
+             """
+         ## 1- ist bei mir so, kann man aber auch einfach ändern.
+         return(1-spatial.distance.cosine(self.ContextVectors[word1].toarray(),self.ContextVectors[word2].toarray()))
+    
+    def getJSD(self, word1, word2):
+        ## Das is irgenwie Käse. Außerdem bekomme ich grundsätzlich
+        ## inf be entropy heraus.
+
+        P = self.ContextVectors[word1].toarray().transpose()[0]
+        Q = self.ContextVectors[word2].toarray().transpose()[0]
+
+        ## Wie bekomme ich eine distribution von P,Q?
+        _P = fix_vec(P / norm(P, ord=1))
+        _Q = fix_vec(Q / norm(Q, ord=1))
+
+        #_M = 0.5 * (_P + _Q)
+        _M = (np.asarray(_P)+np.asarray(_Q)) * 0.5
+
+
+        print(entropy(_Q,_M))
+        # print array with seps
+        #print(','.join(map(str,_M)))
+        
+        ## sqrt for distance, 1- for simularity
+        return (0.5 * (entropy(_P, _M) + entropy(_Q, _M)))
+
+     
+    def isSimilarTo(self, word = "", thres = 0.9,count = 10):
+        """ Returns words with the least distance to the given word.
+        The combination of threshold and count can be used e.g. for 
+        testing to get a small amount of words (and stop after that).
+        
+        :param word:
+        :param thres:
+        :param count
+        :return list of words
+        """
+        if len(word) == 0:
+               return
+        try:
+           # check if ContextVector exists
+           self.ContextVectors[word]
+        except:
+            print("Word not in context")
+            return
+        i = 0
+        for key in self.ContextVectors.keys():                
+            if key != word:
+                sim = self.getJSD(word,key)                
+                if sim != 0:
+                    print(sim)
+                if i == count:
+                    break
+                if sim > thres and i < count:
+                    print(key,sim)
+                    i += 1
+
+
+                    
+
+    def reduceDimensions(self, newDim = 100):
+        """ Converts contextVectors to large matrix 
+        and multiplies it with a random matrix to reduce dim.
+        :param newDim:
+        """
+        ## generate a random matrix, self.k could be different.
+        row = np.array([random.randint(0, self.dim-1) for x in range(self.k)])
+        col = np.array([0 for x in range(self.k)])
+        values = np.array([random.choice([-1, 1]) for x in range(self.k)])
+        randMatrix = sp.coo_matrix((values, (row,col)), (self.dim, newDim))
+        ## convert values of context-vectors to one large matrix (?)
+        vals = self.ContextVectors.values()
+        largeMatrix = sp.csr_matrix((len(vals),self.dim))    
+        i = 0
+        for val in vals:
+            """
+            SparseEfficiencyWarning: Changing the sparsity structure of a csr_matrix is expensive. lil_matrix is more efficient.
+  SparseEfficiencyWarning)
+            """
+            largeMatrix[i] = val.transpose()
+            i += 1
+        reducedMatrix = largeMatrix * randMatrix
+        ## back to dict
+        cKeys = self.ContextVectors.keys()
+        i = 0
+        for key in cKeys:
+            self.ContextVectors[key] = largeMatrix[i].transpose()
+            i += 1
+
+                        
+    def mostSimilar(self,count= 10, file = None ):
+        """ Compare all words in model. (Takes long Time)
+        Isn't that one reason why we need dim-reduction?
+        :param count:
+        :param file:
+        :return:
+        """
+        simDic = []
+        keys = self.ContextVectors.keys()
+        tuples = list(itertools.combinations(keys, 2))
+        print("Comparing everything...")
+
+        i = 0
+        size = len(tuples)
+        for pair in tuples:
+            i+=1
+
+            print("\r%f %%" % (100*i/size), end="")
+            simDic.append([pair[0], pair[1], self.getSimilarityCos(pair[0], pair[1])])
+        print("Sorting...")
+        simDic.sort(key=lambda x: x[2], reverse=True)
+        for x in range(count):
+            print(x, ":\t", simDic[x])
+        if file:
+            with open(file, 'w') as out:
+                for triple in simDic:
+                    out.write(triple[0] + "\t" + triple[1] + "\t" + str(triple[2]) + "\n")
+
+
+                    
 def main():
-	"""Main function if the MOdule gets executed"""
-	r = RIModel(20, 3)
+    """Main function if the Module gets executed"""
+    r = RIModel(100, 3)
+    r.addContext(["hello", "world", "damn"])
+    ## gleiches doc + randomVektor des Dokuments (?)
+    r.addContext(["hello", "world", "damn"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["hello", "world", "damn"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["hello", "damn", "nice"], index=0, mask=[0, 0.5, 0.5])
 
-	r.addContext(["hello", "world", "damn"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["hello", "world", "damn"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["hello", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["hello", "damn", "shitty"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "damn", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "nice"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "nice"], index=0, mask=[0, 0.5, 0.5])
 
-	r.addContext(["the", "damn", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["the", "world", "example"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["the", "world", "shitty"], index=0, mask=[0, 0.5, 0.5])
-	r.addContext(["the", "world", "shitty"], index=0, mask=[0, 0.5, 0.5])
+    r.addContext(["the", "world", "nice"], index=0, mask=[0, 0.5, 0.5])    
+    r.addContext(["parks", "are", "shitty"], index=0, mask=[0, 0.5, 0.5])
+    
 
-	print(r.ContextVectors)
+  #   vals = r.ContextVectors.values()
+  #   largeMatrix = sp.csr_matrix((len(vals),50))    
+  #   i = 0
+  #   for val in vals:
+  #       """
+  #       SparseEfficiencyWarning: Changing the sparsity structure of a csr_matrix is expensive. lil_matrix is more efficient.
+  # SparseEfficiencyWarning)
+  #       """
+  #       largeMatrix[i] = val.transpose()
+  #       i += 1
+                   
+  #   print(largeMatrix)
 
-	for key in r.index_memory.keys():
-		print(key, ":\t", r.index_memory[key].toarray())
+    
+    # for key in r.index_memory.keys():
+    #     print(key, ":\t", r.index_memory[key].toarray())
 
-	print(r.getSimilarityCos("hello", "the"))
-
+    r.getJSD("hello", "parks")
+    ## r.isSimilarTo(word = "hello", thres = 0.1,count = 10)
+    # writeModelToFile(r, 'testModel.pkl')
+    #rmi = loadModelFromFile('testModel.pkl')
+    #print(rmi.getSimilarityCos("hello","the"))
+    
+        
 if __name__ == "__main__":
-	main()
+    main()
