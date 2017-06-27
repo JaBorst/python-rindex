f3aa587e5dc721a614c79eb7e9533d8d0b9d2601
jb <mai13ivw@studserv.uni-leipzig.de>
2017-06-27 13:19:36 +0200

    Added Timeit

--- src/rindex/RIModel.py
+++ src/rindex/RIModel.py
@@ -17,6 +17,21 @@ import operator
 # 						number in input]
 import time
 
+def timeit(method):
+
+    def timed(*args, **kw):
+
+        print("Time Measurement...")
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print ('%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts))
+        return result
+
+    return timed
+
 class RIModel:
 	"""
 	RIModel - Random IndexVector Model
